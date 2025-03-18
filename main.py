f81af6700225f0ae9d063476e39fc373fc56668e
WangRuiming <23302010017@m.fudan.edu.cn>
2025-03-18 14:25:52 +0800

    Adjust main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from argparse import ArgumentParser
 from src.models import load_model, list_models
 from src.benchmarks import load_benchmark, list_benchmarks
-import json
-import os
+from loguru import logger
 
 
 def main():
@@ -11,25 +10,18 @@ def main():
     parser.add_argument('--dataset', type=str, default='voicebench', choices=list_benchmarks())
     parser.add_argument('--subset', type=str, default='alpacaeval')
     parser.add_argument('--split', type=str, default='test')
+    parser.add_argument('--output-dir', type=str, default='output')
     args = parser.parse_args()
 
-    # load data
-    dataset = load_benchmark(benchmark_name=args.dataset, subset_name=args.subset, split=args.split)
+    # load benchmark
+    benchmark = load_benchmark(benchmark_name=args.dataset, subset_name=args.subset, split=args.split)
 
     # load model
     model = load_model(args.model)
 
-    results = dataset.generate(model)
+    results = benchmark.run(model, args.output_dir)
 
-    # save results
-    model_name = args.model.split('/')[-1]
-    output_dir = 'output'
-    os.makedirs(output_dir, exist_ok=True)
-    output_file = os.path.join(output_dir, f'{model_name}-{args.data}-{args.split}-{args.modality}.jsonl')
-    with open(output_file, 'w') as f:
-        for record in results:
-            json_line = json.dumps(record)  # Convert dictionary to JSON string
-            f.write(json_line + '\n')
+    logger.info(results)
 
 
 if __name__ == '__main__':
