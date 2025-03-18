32ca287c6a3362d0f037bf686131573feee67077
WangRuiming <23302010017@m.fudan.edu.cn>
2025-03-18 21:15:39 +0800

    Rename 'dataset' to 'benchmark' (in main.py)

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from src.benchmarks import load_benchmark, list_benchmarks
 def main():
     parser = ArgumentParser()
     parser.add_argument('--model', type=str, default='local', choices=list_models())
-    parser.add_argument('--dataset', type=str, default='voicebench', choices=list_benchmarks())
+    parser.add_argument('--benchmark', type=str, default='voicebench', choices=list_benchmarks())
     parser.add_argument('--subset', type=str, default='alpacaeval')
     parser.add_argument('--split', type=str, default='test')
     parser.add_argument('--output-dir', type=str, default='output')
     args = parser.parse_args()
 
     # load benchmark
-    benchmark = load_benchmark(benchmark_name=args.dataset, subset_name=args.subset, split=args.split)
+    benchmark = load_benchmark(benchmark_name=args.benchmark, subset_name=args.subset, split=args.split)
 
     # load model
     model = load_model(args.model)
