f3334eddc514a95f984f4447b70f515a326ff9f4
WangRuiming <23302010017@m.fudan.edu.cn>
2025-03-21 16:56:13 +0800

    Add whisper large v3 for ASR

---
+++ src/transcriptors/whisper_large_v3.py
@@ -0,0 +1,21 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+class WhisperLargeV3:
+    def __init__(self):
+        self.model_id = "openai/whisper-large-v3"
+        self.model = AutoModelForSpeechSeq2Seq.from_pretrained(
+            self.model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+        ).to(device)
+        self.processor = AutoProcessor.from_pretrained(self.model_id)
+        self.pipe = pipeline(
+            "automatic-speech-recognition",
+            model=self.model,
+            tokenizer=self.processor.tokenizer,
+            feature_extractor=self.processor.feature_extractor,
+            torch_dtype=torch_dtype,
+            device=device,
+        )
