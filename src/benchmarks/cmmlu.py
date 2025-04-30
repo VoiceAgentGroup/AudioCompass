b9108d3b1843b75142dedab99449beb6127ee36d
WangRuiming <2251065331@qq.com>
2025-04-30 17:14:35 +0000

    Fix filename of cmmlu output

--- src/benchmarks/cmmlu.py
+++ src/benchmarks/cmmlu.py
@@ -109,7 +109,7 @@ class CMMLU(BaseBenchmark):
     def save_generated_results(self, results, output_dir, model_name):
         os.makedirs(output_dir, exist_ok=True)
         model_name = model_name.split('/')[-1]
-        output_file = os.path.join(output_dir, f'{model_name}-{self.name}.json')
+        output_file = os.path.join(output_dir, f'{model_name}-{self.name}-{self.split}.json')
         with open(output_file, 'w') as f:
             json.dump(results, f, indent=4)
         logger.info(f"Generated results saved to {output_file}.")    
