344b1b61c48b7d62bfa449a82f17ff7b4bd1b26d
WangRuiming <2251065331@qq.com>
2025-05-05 02:45:39 +0000

    Add a2a for gpt4o

--- src/models/gpt4o.py
+++ src/models/gpt4o.py
@@ -1,5 +1,4 @@
 from .base import VoiceAssistant
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 import transformers
 import torch
 import io
@@ -37,6 +36,23 @@ class GPTAssistant(VoiceAssistant):
             ]
         )
 
+        return completion.choices[0].message.content
+    
+    def generate_a2a(self, audio, max_new_tokens=2048):
+        buffer = io.BytesIO()
+        sf.write(buffer, audio['array'], audio['sampling_rate'], format='WAV')
+        buffer.seek(0)
+        wav_data = buffer.read()
+        encoded_string = base64.b64encode(wav_data).decode('utf-8')
+        completion = self.client.chat.completions.create(
+            model=self.model_name,
+            modalities=["text", "audio"],
+            max_tokens=max_new_tokens,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant who tries to help answer the user's question."},
+                {"role": "user", "content": [{"type": "input_audio", "input_audio": {"data": encoded_string, "format": 'wav'}}]},
+            ]
+        )
         return completion.choices[0].message.content
     
     def asr(self, audio):
