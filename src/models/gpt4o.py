e7b5fd0e2193e488b2c08b99c1b11581adf06b84
WangRuiming <2251065331@qq.com>
2025-05-08 09:50:10 +0800

    Add at2t of gpt4oaudio

--- src/models/gpt4o.py
+++ src/models/gpt4o.py
@@ -54,6 +54,36 @@ class GPTAssistant(VoiceAssistant):
             ]
         )
         return completion.choices[0].message.content
+
+    def generate_at2t(
+        self,
+        audio,
+        text,
+        max_new_tokens=1024,
+    ):
+        # Write the audio data to an in-memory buffer in WAV format
+        buffer = io.BytesIO()
+        sf.write(buffer, audio['array'], audio['sampling_rate'], format='WAV')
+        buffer.seek(0)  # Reset buffer position to the beginning
+
+        # Read buffer as bytes and encode in base64
+        wav_data = buffer.read()
+        encoded_string = base64.b64encode(wav_data).decode('utf-8')
+
+        completion = self.client.chat.completions.create(
+            model=self.model_name,
+            modalities=["text"],
+            max_tokens=max_new_tokens,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant who tries to help answer the user's question."},
+                {"role": "user", "content": [
+                    {"type": "input_audio", "input_audio": {"data": encoded_string, "format": 'wav'}},
+                    {"type": "text", "text": text}
+                ]},
+            ]
+        )
+
+        return completion.choices[0].message.content
     
     def asr(self, audio):
         buffer = io.BytesIO()
