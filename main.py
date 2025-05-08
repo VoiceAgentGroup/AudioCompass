eb8e46d6ec07f79b3c2e87f20917cb11fed0966f
WangRuiming <2251065331@qq.com>
2025-05-08 00:47:55 +0000

    Complement logs in main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from src.benchmarks import load_benchmark, list_benchmarks
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--model-name', type=str, default='baichuan_omni', choices=list_models())
-    parser.add_argument('--benchmark', type=str, default='mmau', choices=list_benchmarks())
+    parser.add_argument('--model-name', type=str, default='checkpoint', choices=list_models())
+    parser.add_argument('--benchmark', type=str, default='commonvoice', choices=list_benchmarks())
     parser.add_argument('--subset', type=str, default='alpacaeval')
-    parser.add_argument('--split', type=str, default='test_mini')
+    parser.add_argument('--split', type=str, default='zh-CN')
     parser.add_argument('--timbre', type=str, default='echo')
     parser.add_argument('--output-dir', type=str, default='output')
     parser.add_argument('--cache-dir', type=str, default='cache')
@@ -21,9 +21,11 @@ def main():
         os.environ['HF_HUB_OFFLINE'] = '1'
 
     # load benchmark
+    logger.info("Loading benchmark ...")
     benchmark = load_benchmark(benchmark_name=args.benchmark, subset_name=args.subset, split=args.split, timbre=args.timbre, cache_dir=args.cache_dir, offline=args.offline)
     
     # load model
+    logger.info("Loading model ...")
     model = load_model(model_name=args.model_name, cache_dir=args.cache_dir, offline=args.offline)
 
     # generate results
