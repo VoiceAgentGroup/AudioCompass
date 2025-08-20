b43d10f0123421ad16da814df7d92bf7c5c349e9
WangRuiming <2251065331@qq.com>
2025-08-20 07:38:45 +0000

    Update cmmlu and mmlu evaluation

--- src/benchmarks/mmlu.py
+++ src/benchmarks/mmlu.py
@@ -6,67 +6,75 @@ import numpy as np
 from .base import BaseBenchmark
 import torchaudio
 import torch
-from utils import gen_mmlu_meta
-
 
 class MMLU(BaseBenchmark):
-    def __init__(self, split, data_dir="datas/mmlu-minimax", cache_dir='cache', **kwargs):
+    def __init__(self, data_dir="datas/mmlu", cache_dir='cache', **kwargs):
         self.name = 'mmlu'
-        self.split = split
         self.data_dir = os.path.join(cache_dir, data_dir)
-        logger.add(f'log/{self.name}-{self.split}.log', rotation='50 MB')
+        import datetime
+        timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        logger.add(f'log/{self.name}-{timestamp}.log', rotation='50MB')
         self.dataset = self.load_data()
 
     def concat_audio(self, question_path, choice_path) -> list:
-        audio_group = []
-
         question_path = os.path.join(self.data_dir, question_path)
         if not os.path.exists(question_path):
             raise ValueError(f"Audio file {question_path} not found")
-        waveform, sample_rate = torchaudio.load(question_path)
-        if waveform.shape[0] > 1:
-            waveform = waveform.mean(dim=0, keepdim=True)
-        question_audio = waveform.squeeze()
+        question_audio, sample_rate = torchaudio.load(question_path)
+        if question_audio.shape[0] > 1:
+            question_audio = question_audio.mean(dim=0, keepdim=True)
+            
+        choice_path = os.path.join(self.data_dir, choice_path)
+        if not os.path.exists(choice_path):
+            raise ValueError(f"Audio file {choice_path} not found")
+        choice_audio, sample_rate = torchaudio.load(choice_path)
+        if choice_audio.shape[0] > 1:
+            choice_audio = choice_audio.mean(dim=0, keepdim=True)
 
-        choice_audios = []
-        for path in choice_path:
-            audio_path = os.path.join(self.data_dir, path)
-            if not os.path.exists(audio_path):
-                raise ValueError(f"Audio file {audio_path} not found")
-            waveform, sample_rate = torchaudio.load(audio_path)
-            if waveform.shape[0] > 1:
-                waveform = waveform.mean(dim=0, keepdim=True)
-            choice_audios.append(waveform.squeeze())
-
-        for choice_audio in choice_audios:
-            complete_audio = torch.cat([question_audio, choice_audio], dim=-1)
-            audio_group.append({
-                'array': complete_audio.numpy(),
-                'sampling_rate': sample_rate,
-            })
+        complete_audio = torch.cat([question_audio, choice_audio], dim=-1)
         
-        return audio_group
+        return complete_audio, sample_rate
+    
+    def concat_with_silence(self, audios, sample_rate):
+        silence = torch.zeros((audios[0].shape[0], int(0.5 * sample_rate)))  # 0.5 second silence
+        combined = audios[0]
+        for segment in audios[1:]:
+            combined = torch.cat((combined, silence, segment), dim=1)
+        return {
+            "array": combined.squeeze(0).numpy(),
+            "sampling_rate": sample_rate
+        }
 
     def load_data(self) -> list:
         logger.info("Preparing data ...")
         dataset = []
-        meta_path = os.path.join(self.data_dir, f"{self.split}_meta_data.json")
-        if not os.path.exists(meta_path):
-            meta_data = gen_mmlu_meta(base_path=self.data_dir, split=self.split)
-        else:
-            with open(meta_path, "r") as f:
-                meta_data = json.load(f)
-        for subject in tqdm(meta_data):
-            data = {'subject': subject['subject'], 'qa': []}
-            for idx, qa in enumerate(subject['qa']):
-                question_path = qa['question']['path']
-                choice_path = [choice['path'] for choice in qa['choice']]
-                audio_group = self.concat_audio(question_path, choice_path)
-                right_answer = qa['right_answer']
-                data['qa'].append({'audio_group': audio_group, 'right_answer': right_answer})
-            dataset.append(data)
+        meta_path = os.path.join(self.data_dir, "meta.json")
+        with open(meta_path, 'r') as f:
+            meta_data = json.load(f)
+        for subject, subject_item in tqdm(list(meta_data.items())[:2], desc="Loading subjects"):
+            data_item = {'subject': subject, 'qa': []}
+            
+            if 'prompt' not in subject_item:
+                continue
+            prompt_path = subject_item['prompt']['path']
+            prompt_audio, _ = torchaudio.load(os.path.join(self.data_dir, prompt_path))
+            if prompt_audio.shape[0] > 1:
+                prompt_audio = prompt_audio.mean(dim=0, keepdim=True)
+                
+            # prepare test audios
+            for test_qa in subject_item['qa']:
+                audio_group = []
+                question_path = test_qa['question']['path']
+                for choice in test_qa['choices']:
+                    choice_path = choice['path']
+                    test_audio, sample_rate = self.concat_audio(question_path, choice_path)
+                    audio = self.concat_with_silence([prompt_audio, test_audio], sample_rate)
+                    audio_group.append(audio)
+                right_answer = test_qa['right_answer']
+                data_item['qa'].append({'audio_group': audio_group, 'right_answer': right_answer})
+            dataset.append(data_item)
         return dataset
-      
+
     def generate(self, model):
         logger.info("Generating results ...")
         results = []
@@ -109,10 +117,10 @@ class MMLU(BaseBenchmark):
     def save_generated_results(self, results, output_dir, model_name):
         os.makedirs(output_dir, exist_ok=True)
         model_name = model_name.split('/')[-1]
-        output_file = os.path.join(output_dir, f'{model_name}-{self.name}-{self.split}.json')
+        output_file = os.path.join(output_dir, f'{model_name}-{self.name}.json')
         with open(output_file, 'w') as f:
             json.dump(results, f, indent=4)
-        logger.info(f"Generated results saved to {output_file}.")    
+        logger.info(f"Generated results saved to {output_file}.")
     
     def run(self, model, output_dir):
         return super().run(model, output_dir)
