8ab0359e54b033002e5632852da98a854929b784
WangRuiming <2251065331@qq.com>
2025-09-11 15:47:33 +0000

    Add environment variable import from .env

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
 from loguru import logger
+from dotenv import load_dotenv
 from argparse import ArgumentParser
 from src.models import load_model, list_models
 from src.benchmarks import load_benchmark, list_benchmarks
 
 
 def main():
+    load_dotenv()
+    
     parser = ArgumentParser()
-    parser.add_argument('--model-name', type=str, default='repulsar_sft', choices=list_models())
-    parser.add_argument('--ckpt', type=str, default="/inspire/hdd/project/embodied-multimodality/public/zxu/megatron_workspace/eval/ckpts/RepulsarSFT-v0.3.2-1/04000")
-    parser.add_argument('--benchmark', type=str, default='mmlu', choices=list_benchmarks())
+    parser.add_argument('--model-name', type=str, default='atlas_sft', choices=list_models())
+    parser.add_argument('--ckpt', type=str, default="/inspire/hdd/project/embodied-multimodality/public/rmwang/AudioCompass/cache/models/ckpts/Atlas-SFT-v1.2.0/0040000")
+    parser.add_argument('--benchmark', type=str, default='voxeval', choices=list_benchmarks())
     parser.add_argument('--subset', type=str, default='mmsu')
     parser.add_argument('--split', type=str, default='test')
-    parser.add_argument('--timbre', type=str, default='echo')
+    parser.add_argument('--timbre', type=str, default='alloy')
     parser.add_argument('--output-dir', type=str, default='output')
     parser.add_argument('--cache-dir', type=str, default='cache')
     parser.add_argument('--offline', action="store_true")
